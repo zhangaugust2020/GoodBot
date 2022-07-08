276b9c2e6a0c69bc80afcc980b51166f10e64016
我是一只小猪熊 <939656378@qq.com>
2022-07-08 13:10:58 +0800

    项目骨架搭建好了

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -4,11 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author August
  * @date 2022/7/8 - 10:40
  */
-@Controller
+@RestController
 @RequestMapping("/pk")
 public class IndexController {
+
+    @RequestMapping("/getBotInfo")
+    public Map<String, String> getBotInfo() {
+        Map<String, String> map = new HashMap();
+        map.put("name", "1");
+        map.put("rating", "2");
+        return map;
+    }
+
 }
