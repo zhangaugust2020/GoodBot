6956e46d636fcf167f670f09b30cd418a41e8b7f
我是一只小猪熊 <939656378@qq.com>
2022-07-08 10:47:34 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -0,0 +1,14 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author August
+ * @date 2022/7/8 - 10:40
+ */
+@Controller
+@RequestMapping("/pk")
+public class IndexController {
+}
